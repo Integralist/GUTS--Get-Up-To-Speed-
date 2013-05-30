83d689fa10e2829b81b1e75a2e9fbb18e8a47bcf
Mark McDonnell <mark.mcdx@gmail.com>
2013-05-30 19:13:39 +0100

    Refactoring

--- Assets/Scripts/app.js
+++ Assets/Scripts/app.js
@@ -3,6 +3,98 @@ guts.GROUP2 = 599;
 guts.GROUP3 = 1007;
 guts.GROUP4 = 1425; // an arbitrary value to indicate desktop and higher
 
+guts.generateComponentView = function(){
+    var container    = document.querySelector('.container'),
+        iframe       = document.createElement('iframe');
+        iframe.src   = 'components/object-media.html';
+    
+    iframe.onload = function(){
+        guts.pubsub.publish('component:loaded');
+    };
+
+    this.iframe = iframe;
+
+    document.body.appendChild(iframe);
+};
+
+guts.resizeUnitTestsHeight = function(){
+    iframe = this.unitTestsIframe;
+    iframe.style.height = 0; // WebKit returns the current `scrollHeight`, so we reset to zero to allow the content of the iframe to determine the height
+    iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 30) + 'px'; // Firefox doesn't resize properly so we need to add some additional space
+
+    this.pubsub.publish('unit-tests:height:set');
+};
+
+guts.checkUnitTestIframeCreated = function(){
+    if (this.unitTestsIframe) {
+        this.toggleUnitTestIframeVisibility();
+    }
+};
+
+guts.resizeComponentHeight = function(){
+    iframe = this.iframe;
+    iframe.style.height = 0; // WebKit returns the current `scrollHeight`, so we reset to zero to allow the content of the iframe to determine the height
+    iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 30) + 'px'; // Firefox doesn't resize properly so we need to add some additional space
+
+    this.pubsub.publish('component:height:set');
+};
+
+guts.componentLoaded = function(){
+    var screenWidth = document.documentElement.clientWidth,
+        setWidthTo = (screenWidth < this.GROUP1) ? screenWidth : this.GROUP1 /* default size to load when the app initializes */;
+    
+    this.resizeIframeWidth(setWidthTo);
+};
+
+guts.handleResize = function(){
+    this.resizeIframeWidth(document.documentElement.clientWidth);
+};
+
+guts.createUnitTestIframe = function(){
+    var iframe            = document.createElement('iframe');
+        iframe.src        = 'unit-tests/SpecRunner.html';
+        iframe.className  = 'unit-tests';
+
+    iframe.onload = function(){
+        guts.pubsub.publish('unit-tests:loaded');
+    };
+    
+    this.unitTestsIframe = iframe;
+
+    document.body.appendChild(iframe);
+};
+
+guts.toggleUnitTestIframeVisibility = function(){
+    var button = document.getElementById('js-unit-tests-value');
+
+    if (button.checked) {
+        this.unitTestsIframe.classList.remove('hide');
+    } else {
+        this.unitTestsIframe.classList.add('hide');
+    }
+};
+
+guts.handleUnitTestDisplay = function(){
+    if (this.unitTestsIframe) {
+        this.toggleUnitTestIframeVisibility();
+    } else {
+        this.createUnitTestIframe();
+    }
+};
+
+guts.handleCustomResize = function(){
+    var button = document.getElementById('js-resize-value'),
+        validNumericalValue  = /\d+/.exec(button.value);
+    
+    if (validNumericalValue) {
+        this.resizeIframeWidth(validNumericalValue);
+    }
+};
+
+guts.loadComponent = function(input) {
+    this.iframe.src = 'components/' + input.value + '.html';
+};
+
 guts.determineGroupWidth = function(group) {
     var width;
 
@@ -24,19 +116,15 @@ guts.determineGroupWidth = function(group) {
     return width;
 };
 
-guts.loadComponent = function(input) {
-    this.iframe.src = 'components/' + input.value + '.html';
-};
+guts.resizeIframeWidth = function(width) {
+    if (this.iframe) {
+        this.iframe.style.width = width + 'px';
+        this.pubsub.publish('component:width:set');
+    }
 
-guts.checkInputSelected = function(input) {
-    if (input.type === 'radio') {
-        if (input.name === 'groups') {
-            this.resizeIframeWidth(this.determineGroupWidth(input.value));
-        }
-
-        if (input.name === 'components') {
-            this.loadComponent(input);
-        }
+    if (this.unitTestsIframe) {
+        this.unitTestsIframe.style.width = width + 'px';
+        this.pubsub.publish('unit-tests:width:set');
     }
 };
 
@@ -51,50 +139,15 @@ guts.checkButtonSelected = function(input) {
     }
 };
 
-guts.handleCustomResize = function(){
-    var button = document.getElementById('js-resize-value'),
-        validNumericalValue  = /\d+/.exec(button.value);
-    
-    if (validNumericalValue) {
-        this.resizeIframeWidth(validNumericalValue);
-    }
-};
+guts.checkInputSelected = function(input) {
+    if (input.type === 'radio') {
+        if (input.name === 'groups') {
+            this.resizeIframeWidth(this.determineGroupWidth(input.value));
+        }
 
-guts.handleUnitTestDisplay = function(){
-    if (this.unitTestsIframe) {
-        this.toggleUnitTestIframeVisibility();
-    } else {
-        this.createUnitTestIframe();
-    }
-};
-
-guts.toggleUnitTestIframeVisibility = function(){
-    var button = document.getElementById('js-unit-tests-value');
-
-    if (button.checked) {
-        this.unitTestsIframe.classList.remove('hide');
-    } else {
-        this.unitTestsIframe.classList.add('hide');
-    }
-};
-
-guts.createUnitTestIframe = function(){
-    var iframe            = document.createElement('iframe');
-        iframe.src        = 'unit-tests/SpecRunner.html';
-        iframe.className  = 'unit-tests';
-
-    iframe.onload = function(){
-        guts.pubsub.publish('unit-tests:loaded');
-    };
-    
-    this.unitTestsIframe = iframe;
-
-    document.body.appendChild(iframe);
-};
-
-guts.checkUnitTestIframeCreated = function(){
-    if (this.unitTestsIframe) {
-        this.toggleUnitTestIframeVisibility();
+        if (input.name === 'components') {
+            this.loadComponent(input);
+        }
     }
 };
 
@@ -109,59 +162,6 @@ guts.delegationHandler = function(e) {
     if (tag === 'button') {
         this.checkButtonSelected(target);
     }
-};
-
-guts.resizeIframeWidth = function(width) {
-    if (this.iframe) {
-        this.iframe.style.width = width + 'px';
-        this.pubsub.publish('component:width:set');
-    }
-
-    if (this.unitTestsIframe) {
-        this.unitTestsIframe.style.width = width + 'px';
-        this.pubsub.publish('unit-tests:width:set');
-    }
-};
-
-guts.resizeComponentHeight = function(){
-    iframe = this.iframe;
-    iframe.style.height = 0; // WebKit returns the current `scrollHeight`, so we reset to zero to allow the content of the iframe to determine the height
-    iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 30) + 'px'; // Firefox doesn't resize properly so we need to add some additional space
-
-    this.pubsub.publish('component:height:set');
-};
-
-guts.resizeUnitTestsHeight = function(){
-    iframe = this.unitTestsIframe;
-    iframe.style.height = 0; // WebKit returns the current `scrollHeight`, so we reset to zero to allow the content of the iframe to determine the height
-    iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 30) + 'px'; // Firefox doesn't resize properly so we need to add some additional space
-
-    this.pubsub.publish('unit-tests:height:set');
-};
-
-guts.componentLoaded = function(){
-    var screenWidth = document.documentElement.clientWidth,
-        setWidthTo = (screenWidth < this.GROUP1) ? screenWidth : this.GROUP1 /* default size to load when the app initializes */;
-    
-    this.resizeIframeWidth(setWidthTo);
-};
-
-guts.handleResize = function(){
-    this.resizeIframeWidth(document.documentElement.clientWidth);
-};
-
-guts.generateComponentView = function(){
-    var container    = document.querySelector('.container'),
-        iframe       = document.createElement('iframe');
-        iframe.src   = 'components/object-media.html';
-    
-    iframe.onload = function(){
-        guts.pubsub.publish('component:loaded');
-    };
-
-    this.iframe = iframe;
-
-    document.body.appendChild(iframe);
 };
 
 guts.bindEvents = function(){
--- unit-tests/spec/AppSpec.js
+++ unit-tests/spec/AppSpec.js
@@ -1,7 +1,137 @@
 describe('App', function(){
     describe('init', function(){
+        it('should be defined', function(){
+            //
+        });
+
         it('should call all required methods', function(){
             //
         });
-    })
+    });
+
+    describe('bindEvents', function(){
+        it('should subscribe to multiple custom events', function(){
+            //
+        });
+
+        it('should bind a click event handler to the document body', function(){
+            //
+        });
+
+        it('should bind a resize event handler to the window', function(){
+            //
+        });
+    });
+
+    describe('delegationHandler', function(){
+        it('should call `checkInputSelected` if an input element is provided', function(){
+            //
+        });
+
+        it('should call `checkButtonSelected` if a button element is provided', function(){
+            //
+        });
+    });
+
+    describe('checkInputSelected', function(){
+        it('should call both `determineGroupWidth` and `resizeIframeWidth` if input type is "radio" and the name is "group"', function(){
+            //
+        });
+
+        it('should call `loadComponent` if input type is "radio" and the name is "components"', function(){
+            //
+        });
+    });
+
+    describe('checkButtonSelected', function(){
+        it('should call `handleCustomResize` if the button id is "js-resize"', function(){
+            //
+        });
+
+        it('should call `handleUnitTestDisplay` if the button id is "js-unit-tests"', function(){
+            //
+        });
+    });
+
+    describe('resizeIframeWidth', function(){
+        it('should publish a custom event specifically for the main component iframe', function(){
+            //
+        });
+
+        it('should publish a custom event specifically for the unit tests iframe', function(){
+            //
+        });
+    });
+
+    describe('determineGroupWidth', function(){
+        it('should return the correct numerical value for the specified group', function(){
+            //
+        });
+    });
+
+    describe('loadComponent', function(){
+        it('should successfully set the "src" attribute of the iframe', function(){
+            //
+        });
+    });
+
+    describe('handleCustomResize', function(){
+        it('should call `resizeIframeWidth` if a valid numerical value is specified by the user', function(){
+            //
+        });
+    });
+
+    describe('handleUnitTestDisplay', function(){
+        it('should ...', function(){
+            //
+        });
+    });
+
+    describe('toggleUnitTestIframeVisibility', function(){
+        it('should ...', function(){
+            //
+        });
+    });
+
+    describe('createUnitTestIframe', function(){
+        it('should ...', function(){
+            //
+        });
+    });
+
+    describe('handleResize', function(){
+        it('should ...', function(){
+            //
+        });
+    });
+
+    describe('componentLoaded', function(){
+        it('should ...', function(){
+            //
+        });
+    });
+
+    describe('resizeComponentHeight', function(){
+        it('should ...', function(){
+            //
+        });
+    });
+
+    describe('checkUnitTestIframeCreated', function(){
+        it('should ...', function(){
+            //
+        });
+    });
+
+    describe('resizeUnitTestsHeight', function(){
+        it('should ...', function(){
+            //
+        });
+    });
+
+    describe('generateComponentView', function(){
+        it('should ...', function(){
+            //
+        });
+    });
 });
